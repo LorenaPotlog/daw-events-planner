50f2a91c0fb09444a5ba6f2b1cd3801595e2dc9c
Lorena Potlog <101601637+LorenaPotlog@users.noreply.github.com>
2024-01-06 17:31:46 +0200

    Update db_connect.php

--- src/inc/db_connect.php
+++ src/inc/db_connect.php
@@ -1,9 +1,9 @@
 <?php
 function getDBConnection() {
-    $servername = "localhost"; // Replace with your server name
-    $username = "root"; // Replace with your username
-    $password = ""; // Replace with your password
-    $database = "auth"; // Replace with your database name
+    $servername = "nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306"; // Replace with your server name
+    $username = "f0vxhqmqtwqdl27i"; // Replace with your username
+    $password = "le4opdqp4c7ke9ao"; // Replace with your password
+    $database = "ma7qaz0cxsi0en6h"; // Replace with your database name
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
