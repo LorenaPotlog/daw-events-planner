aa8a959c9be03267335309bede4ddfbd4ce0abda
Lorena Potlog <lorena.potlog@yahoo.com>
2024-01-13 23:52:27 +0200

    fix register

--- public/register.php
+++ public/register.php
@@ -13,7 +13,9 @@ require __DIR__ . '/../src/user/register.php';
             max-width: 400px;
             width: 100%;
             box-sizing: border-box;
-            margin-left: 600px;
+            margin-right: auto;
+            margin-left: auto;
+            margin-bottom: 3%;
         }
     </style>
     <div id="page-container">
@@ -23,32 +25,45 @@ require __DIR__ . '/../src/user/register.php';
 
                 <div>
                     <label for="firstname">First name:</label>
-                    <input type="text" name="firstname" id="firstname" value="<?= isset($_SESSION['inputs']['firstname']) ? htmlspecialchars($_SESSION['inputs']['firstname']) : '' ?>" class="form-input text-center">
+                    <input type="text" name="firstname" id="firstname"
+                           value="<?= isset($_SESSION['inputs']['firstname']) ? htmlspecialchars($_SESSION['inputs']['firstname']) : '' ?>"
+                           class="form-input text-center">
                 </div>
                 <div>
                     <label for="lastname">Last name:</label>
-                    <input type="text" name="lastname" id="lastname" value="<?= isset($_SESSION['inputs']['lastname']) ? htmlspecialchars($_SESSION['inputs']['lastname']) : '' ?>" class="form-input text-center">
+                    <input type="text" name="lastname" id="lastname"
+                           value="<?= isset($_SESSION['inputs']['lastname']) ? htmlspecialchars($_SESSION['inputs']['lastname']) : '' ?>"
+                           class="form-input text-center">
                 </div>
                 <div>
                     <label for="username">Username:</label>
-                    <input type="text" name="username" id="username" value="<?= isset($_SESSION['inputs']['username']) ? htmlspecialchars($_SESSION['inputs']['username']) : '' ?>" class="form-input text-center">
+                    <input type="text" name="username" id="username"
+                           value="<?= isset($_SESSION['inputs']['username']) ? htmlspecialchars($_SESSION['inputs']['username']) : '' ?>"
+                           class="form-input text-center">
                 </div>
                 <div>
                     <label for="email">Email:</label>
-                    <input type="email" name="email" id="email" value="<?= isset($_SESSION['inputs']['email']) ? htmlspecialchars($_SESSION['inputs']['email']) : '' ?>" class="form-input text-center">
+                    <input type="email" name="email" id="email"
+                           value="<?= isset($_SESSION['inputs']['email']) ? htmlspecialchars($_SESSION['inputs']['email']) : '' ?>"
+                           class="form-input text-center">
                 </div>
                 <div>
                     <label for="password">Password:</label>
-                    <input type="password" name="password" id="password" value="<?= isset($_SESSION['inputs']['password']) ? htmlspecialchars($_SESSION['inputs']['password']) : '' ?>" class="form-input text-center">
+                    <input type="password" name="password" id="password"
+                           value="<?= isset($_SESSION['inputs']['password']) ? htmlspecialchars($_SESSION['inputs']['password']) : '' ?>"
+                           class="form-input text-center">
                 </div>
                 <div>
                     <label for="password2">Password Again:</label>
-                    <input type="password" name="password2" id="password2" value="<?= isset($_SESSION['inputs']['password2']) ? htmlspecialchars($_SESSION['inputs']['password2']) : '' ?>" class="form-input text-center">
+                    <input type="password" name="password2" id="password2"
+                           value="<?= isset($_SESSION['inputs']['password2']) ? htmlspecialchars($_SESSION['inputs']['password2']) : '' ?>"
+                           class="form-input text-center">
                 </div>
 
                 <div class="checkbox-group">
                     <label for="agree" style="text-align: center">I agree with the <a>terms of services.</a></label>
-                    <input type="checkbox" name="agree" id="agree" <?= isset($_SESSION['inputs']['agree']) && $_SESSION['inputs']['agree'] ? 'checked' : '' ?>>
+                    <input type="checkbox" name="agree"
+                           id="agree" <?= isset($_SESSION['inputs']['agree']) && $_SESSION['inputs']['agree'] ? 'checked' : '' ?>>
                 </div>
 
                 <button type="submit" name="register">Register</button>
